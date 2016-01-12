28b8a8b51f1c4f8295d061c1af691f172aae1321
Owen Evans <owen@bgeek.net>
2016-01-12 13:50:48 +1300

    fix poll/webhook setup

--- src/poll.js
+++ src/poll.js
@@ -48,8 +48,8 @@ class GitLabPoller {
           this._context.subscription.delayTill(new Moment().add(1, 'hour').toDate());
           throw new ConnectorRequiresAuthorizationError();
         }
-        if (!this._context.authorization.get('SubscriptionRepository')) {
-          this._logger.warn('Connector needs a subscription repository and none set');
+        if (!this._context.authorization.get('SubscriptionProject')) {
+          this._logger.warn('Connector needs a subscription project and none set');
           //we've already setup this subscription
           this._context.subscription.delayTill(new Moment().add(1, 'hour').toDate());
           throw new ConnectorRequiresAuthorizationError();
@@ -65,7 +65,7 @@ class GitLabPoller {
         return this._connector.authorize(this._context.authorization);
       }).then(() => {
         this._logger.info('creating webhook endpoint');
-        let hookUri = `https://${config.get('Hoist.domains.endpoint')}/connector/${this._context.authorization.key}`;
+        let hookUri = `https://${config.get('Hoist.domains.endpoint')}/connector/${this._context.authorization._token.key}`;
         this._connector.post(`/projects/${this._context.authorization.get('SubscriptionProject')}/hooks`, {
           "url":hookUri,
           "push_events": true,
